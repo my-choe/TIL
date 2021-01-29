52f4ca5fcd2256edeccb59679bc920e0512e9ef9
Minyoung Choi <my_choe@naver.com>
2021-01-29 22:14:20 +0900

    Delete Python/WebScrapping/Indeed directory

--- Python/WebScrapping/Indeed/main.py
+++
@@ -1,8 +0,0 @@
-#https://kr.indeed.com/ 사이트에서 python 개발자 구인공고 스크래핑하기
-from indeed import extract_indeed_pages, extract_indeed_jobs
-
-last_indeed_pages = extract_indeed_pages()
-
-indeed_jobs = extract_indeed_jobs(last_indeed_pages)
-
-print(indeed_jobs)
