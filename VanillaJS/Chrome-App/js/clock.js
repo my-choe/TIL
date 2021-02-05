8db9fec39289960fd2d89446ea56e11a3f9e4503
Minyoung Choi <my_choe@naver.com>
2021-02-05 17:12:28 +0900

    [update] clock.js

--- VanillaJS/Chrome-App/js/clock.js
+++ VanillaJS/Chrome-App/js/clock.js
@@ -6,13 +6,15 @@ function getTime() {
     const hours = date.getHours();
     const minutes= date.getMinutes();
     const seconds = date.getSeconds();
-    
-    clockTitle.innerText = `${hours < 10 ? `0${hours}`:hours}:${minutes < 10 ? `0${minutes}`:minutes}:${seconds < 10 ? `0${seconds}`:seconds}`;
+    const time = `${hours < 10 ? `0${hours}`:hours}:${minutes < 10 ? `0${minutes}`:minutes}:${seconds < 10 ? `0${seconds}`:seconds}`;
+    clockTitle.innerText = time;
+    return;
 }
 
 function init() {
     getTime();
     setInterval(getTime, 1000);
+    return;
 }
 
 init();
